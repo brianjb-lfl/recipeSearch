e1fe53e4b39e1d277a4912bce9145d9f1020a828
Brian Besaw <bjbesaw@outlook.com>
2017-08-31 12:58:39 -0400

    store added

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 'use strict';
-/* <button type="submit" id='gen-search-btn'>Search</button>
-<button type="button" id="get-adv-search">Adv Search</button>
-<button type="button" id="results-filter">Next</button>
-<button type="button" id="results-sort">Next</button>
-<button type="button" id="results-new-search">Next</button>
-<button type="button" id="results-prev-btn">Next</button>
-<button type="button" id="results-next-btn">Next</button> */
-{/* <form id='adv-search-form'>
-<button type="button" id="add-ingredient-button">Add</button>
-<button type="button" id="exclude-ingredient-button">Exclude</button>
-<button type="submit" id="submit-adv-search-button">Go Search</button>  */}
-//          <div id="results-button-ctr">
-
-{/* <div class='panel' id='search-main'>
-<form id='gen-search-form'>
-<input id='search-input'></input>
-<div id='recipe-of-the-day'>
-<div class='panel' id='adv-search'>
-<div class='panel' id='search-results'>
-<div id='results-container'> */}
 
 const SEARCH_URL = 'https://api.edamam.com/search';
 const API_ID = '78cf2ab0';
 const API_KEY = '92dd621055244e3a5dad9c0b3e409002'; // later this should be secure
+
+const STORE = {
+
+};
 
 const EL = {
   genSrchInput: $('#search-input'),
