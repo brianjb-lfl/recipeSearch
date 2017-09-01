1087353067ee2d3b9be1fad46cdfc407b04875ff
bradcgarner <brad@bradgarner.com>
2017-09-01 02:02:44 -0400

    brad did sort, filter, render results to html

--- app.js
+++ app.js
@@ -28,14 +28,45 @@ const EL = {
 
   results: $('#search-results'), // overall panel
   resultsCntr: $('#results-container'), // where the list goes inside panel
+  resultHeader: $('#search-results-header'),
   resFiltBtn: $('#results-filter'),
   resSortBtn: $('#results-sort'),
   resPrevBtn: $('#results-prev-btn'),
   resNextBtn: $('#results-next-btn'),
 };
 
-function resSort () {
-  
+function resSort (prop) {
+  STORE.results.sort((a,b)=>{
+    if (a.recipe[prop] < b.recipe[prop]) {
+      return -1;
+    } else if (a.recipe[prop] > b.recipe[prop]) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  render();
+}
+
+function resFilt (prop, value, comparison) {
+  // change this to be case in-sensitive
+  // use prop.search(value) for word/character searches, returns -1 if no match
+  let newArr = [];
+  if (comparison ==='includes') {
+    newArr = STORE.results.filter(item=>item.recipe[prop].includes(value));    
+  } else if ( comparison ==='excludes') {
+    newArr = STORE.results.filter(item=>!item.recipe[prop].includes(value));  
+  } else if ( comparison ==='greater') {
+    newArr = STORE.results.filter(item=>item.recipe[prop] >= value);  
+  } else if ( comparison ==='less') {
+    newArr = STORE.results.filter(item=>item.recipe[prop] <= value);  
+  } else if ( comparison ==='not') {
+    newArr = STORE.results.filter(item=>item.recipe[prop] !== value); 
+  } else {
+    newArr = STORE.results.filter(item=>item.recipe[prop] === value);  
+  }
+  STORE.results = newArr.slice();
+  render();
 }
 
 function resPrev () {
@@ -44,6 +75,103 @@ function resPrev () {
 
 function resNext () {
   
+}  
+
+function resetStore(){
+  delete STORE.results;
+  delete STORE.query;
+} 
+
+function formatStoreHtml() {
+  STORE.results.forEach((cv,idx)=>{
+    STORE.results[idx].recipe.details = 
+      `<div class='results-recipe-details'>
+      <h3 class='results-recipe-name'>${cv.recipe.label}</h3>
+      <p>Serves ${cv.recipe.yield}</p>
+      <div class='results-recipe-image-container'>
+        <a href="${cv.recipe.url}" target="_blank"><img src="${cv.recipe.image}"/></a>
+      </div> 
+      <div class='results-recipe-details-full'>
+        <div class='results-recipe-cals-brief'>
+          <h4>${cv.recipe.calsServing}</h4>
+          <p>per serving</p>
+        </div>
+        <div class='results-recipe-ingredients'>
+          <ul class='results-recipe-ingredients'>Ingredients:${cv.recipe.ingredientLi.join(' ')}</ul>
+        </div><!--end results-recipe-ingredients-->
+        <div class='results-recipe-health'>
+          <ul class='results-recipe-health'>Health Criteria:${cv.recipe.healthLi.join(' ')}</ul>
+        </div><!--end results-recipe-health-->
+        <div class='results-recipe-diet'>
+          <ul class='results-recipe-diet'>Dietary Restrictions:${cv.recipe.dietLi.join(' ')}</ul>
+        </div><!--end results-recipe-diet-->
+        <div class='results-recipe-cautions'>
+          <ul class='results-recipe-cautions'>Cautions:${cv.recipe.cautionsLi.join(' ')}</ul>
+        </div><!--end results-recipe-cautions-->
+        <div class='results-recipe-nutrTable'>
+          <h6>Nutrition Information:</h6>${cv.recipe.nutrTable}
+        </div><!--end results-recipe-nutrTable-->
+      </div><!--end results-recipe-details-full-->
+    </div><!--end results-recipe-list-->`;
+  
+  STORE.results[idx].recipe.htmlList =
+    `<div class='results-recipe-list'>
+      <h3 class='results-recipe-name'>${cv.recipe.label}</h3>
+      <p>Serves ${cv.recipe.yield}</p>
+      <div class='results-recipe-thumbnail-container'>
+        <a href="${cv.recipe.url}" target="_blank"><img src="${cv.recipe.image}"/></a>
+      </div> 
+      <div class='results-recipe-details-brief'>
+         <div class='results-recipe-cals-brief'>
+          <h4>${cv.recipe.calsServing}</h4>
+          <p>per serving</p>
+        </div>
+        <div class='results-recipe-ingredients'>
+          <ul class='results-recipe-ingredients'>
+            ${cv.recipe.ingredientLi[0]}
+            ${cv.recipe.ingredientLi[1]}
+            ${cv.recipe.ingredientLi[2]}
+           </ul>
+          <p class='seeMore'>...see more</p>        
+        </div><!--end results-recipe-ingredients-->
+      </div><!--end results-recipe-details-brief-->
+      ${STORE.results[idx].recipe.details}
+    </div><!--end results-recipe-list-->`;
+    // not currently using cv.recipe.shareAs
+  }); // end forEach
+}
+
+// reads raw data from STORE and manipulates it for use in rendering (saves in store, doesn't render)
+function preformatStore(){
+  STORE.results.forEach((cv,idx)=>{
+    // combine totalDaily and totalNutrients
+    // STORE.results[idx].recipe.nutrition = {};
+    for (let prop in cv.recipe.totalDaily) {
+      STORE.results[idx].recipe.totalDaily[prop].labelNutr = STORE.results[idx].recipe.totalNutrients[prop].label;
+      STORE.results[idx].recipe.totalDaily[prop].quantityNutr = STORE.results[idx].recipe.totalNutrients[prop].quantity;
+      STORE.results[idx].recipe.totalDaily[prop].unitNutr = STORE.results[idx].recipe.totalNutrients[prop].unit;
+    }
+    // convert nutritiion into an html table
+    let trTd = '';
+    for (let prop in cv.recipe.totalDaily){
+      trTd += `<tr>
+      <td>${cv.recipe.totalDaily[prop].label}</td>
+      <td>${cv.recipe.totalDaily[prop].quantityNutr}</td>
+      <td>${cv.recipe.totalDaily[prop].unitNutr}</td>
+      <td>${cv.recipe.totalDaily[prop].quantity}</td>
+      <td>${cv.recipe.totalDaily[prop].unit}</td>
+      </tr>`;
+    }
+
+    let th = '<tr><th>Nutrient</th><th>Qty/Serving</th><th>Unit</th><th>Daily Value</th><th>% Daily Value</th></tr>';
+    STORE.results[idx].recipe.nutrTable = `<table class='results-recipe-nutrTable'> ${th} ${trTd} </table>`;
+    // convert list items into html li
+    STORE.results[idx].recipe.ingredientLi = cv.recipe.ingredientLines.map((x,idx) => `<li>${cv.recipe.ingredientLines[idx]}</li>`);
+    STORE.results[idx].recipe.healthLi = cv.recipe.healthLabels.map((x,idx) => `<li>${cv.recipe.healthLabels[idx]}</li>`);
+    STORE.results[idx].recipe.dietLi = cv.recipe.dietLabels.map((x,idx) => `<li>${cv.recipe.dietLabels[idx]}</li>`);
+    STORE.results[idx].recipe.cautionsLi = cv.recipe.cautions.map((x,idx) => `<li>${cv.recipe.cautions[idx]}</li>`);
+    STORE.results[idx].recipe.calsServing = cv.recipe.calories/cv.recipe.yield;
+  }); // end map
 }
 
 // get advanced search data from drop-down menu options
@@ -53,8 +181,41 @@ function resNext () {
 function loadResults(data) {
   STORE.results = data.hits.slice();
   STORE.query = data.params; // use a filter not to store app_id, app_key, sane...  
+  preformatStore();
+  formatStoreHtml();
   render();
 }
+
+function loadRecipeOfDay(data) {
+  STORE.results = data.hits.slice();
+  STORE.query = data.params; // use a filter not to store app_id, app_key, sane...  
+  preformatStore(); // do exactly the same as the store to avoid re-writing that
+  formatStoreHtml();
+  STORE.recipeOfDay = Object.assign({},STORE.results[0].recipe); // copy recipe of day to a separate part of store
+  renderRecipeOfDay(); // render to screen
+  resetStore(); 
+}
+
+function getRecipeOfDay() {
+  let theDate = new Date();
+  let m = parseInt(theDate.getMonth()); 
+  let d = parseInt(theDate.getDate());
+  let d2 = d++;
+  let w = parseInt(theDate.getDay());
+  let topic = ['lemon','pear','berry','egg','grill','summer','green','roast','bake','squash','harvest','rich','red'];
+  let health = ['vegan','tree-nut-free','vegetarian','peanut-free','vegan','tree-nut-free','vegetarian','peanut-free','peanut-free'];
+  let i = m; // index (topic) is per month
+  if (w===2) {i--;} else if (w>5) {i++;} // Tues, use last month's topic, Fri-Sat use next month's
+  const query = {
+    q: topic[i],
+    app_id: API_ID,
+    app_key: API_KEY,
+    health: health[4],
+    from: 0,
+    to: 5
+  };
+  $.getJSON(SEARCH_URL, query, loadRecipeOfDay);
+};
 
 // 3 CALLED FROM 2 watchSubmit() >>> RECEIVE QUERY & CALLBACK FUNCTION 4
 function getResultsFromApi(searchTerm, callback) {
@@ -101,12 +262,23 @@ function watchSubmit() {
   });
   EL.resNewSrchBtn.click(event=>{
     STORE.appState = 'genSrch';
-    // clear the store
+    resetStore();
     render();
-  })
+  });
+  EL.resSortBtn.click(event=>{
+    STORE.resultsPage = 1;
+    resSort('label'); // edit this so that the property is read from the button or a checkbox
+    render();
+  });
+  EL.resFiltBtn.click(event=>{
+    STORE.resultsPage = 1;
+    resFilt('label', 'Lemon Pudding', 'includes'); // edit this so that the property is read from the button or a checkbox
+    render();
+  });
 }
 
 // 1 DOCUMENT READY
+$(getRecipeOfDay);
 $(watchSubmit);
 
 
--- render.js
+++ render.js
@@ -1,17 +1,41 @@
 'use strict';
 
+function renderRecipeOfDay(){
+  EL.recOfDay.html(STORE.recipeOfDay.htmlList);
+}
+
+function renderResultsHeader(){
+  let searchCtHeader = `<h4>${STORE.results.length} found, showing ${STORE.query.from} to ${STORE.query.to}<h4>` ;
+  let query = (STORE.query.q) ? `<li>Search Term: ${STORE.query.q}</li>` : '' ;
+  let health = (STORE.query.health) ? `<li>Health Criteria: ${STORE.query.health.join(', ')}</li>` : '' ;
+  let diet = (STORE.query.diet) ? `<li>Dietary Restrictions: ${STORE.query.diet.join(', ')}</li>` : '' ;
+  let cals = (STORE.query.calories) ? `<li>Calories: ${STORE.query.calories}</li>` : '' ;
+  
+  let searchQueryReadout = `<p>You searched for:</p> <ul> ${query} ${health} ${diet} ${cals} </ul>`;
+  EL.resultHeader.html(`${searchCtHeader} ${searchQueryReadout}`);
+}
+
+function renderResults(){
+  let html = '';
+  STORE.results.forEach(cv=>html += cv.recipe.htmlList);
+  EL.resultsCntr.html(html);
+}
+
 function render () {
   EL.panel.addClass('hidden'); // hide all panels
   if (STORE.appState === 'advSrch') {
     EL.advSrch.removeClass('hidden');
-  } else if (STORE.appState === 'results') {    
-    let html = '';
-    STORE.results.map((cv,idx)=>{
-      html += `<li>${cv.recipe.label}</li>`;
-    });
-    EL.resultsCntr.html(html);
+  } else if (STORE.appState === 'results') {  
+    renderResultsHeader();  
+    renderResults();
+    // let html = '';
+    // STORE.results.map((cv,idx)=>{
+    //   html += `<li>${cv.recipe.label}</li>`;
+    // });
+    // EL.resultsCntr.html(html);
     EL.results.removeClass('hidden');
   } else { // appstate === genSrch
+    renderRecipeOfDay();
     EL.genSrch.removeClass('hidden');    
   }  
 }
